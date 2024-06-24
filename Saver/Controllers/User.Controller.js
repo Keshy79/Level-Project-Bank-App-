5c110f996cd7313433467b48ff4296faad0903ed
Okunade Keshifat Iyabo <keshifatokunade@gmail.com>
2024-06-24 15:06:32 +0100

    Deployment (fixing error)

--- Saver/Controllers/User.Controller.js
+++ Saver/Controllers/User.Controller.js
@@ -1,4 +1,4 @@
-const { userModel, createReservedAcct } = require("../Models/User.Model");
+const { userModel, createReservedAcct } = require("../Models/user.model");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 require("dotenv").config();
